684bf4ba58ce97177841a8f0043e9f24bec9dbed
Serhii Bykov <esphynox@gmail.com>
2026-07-24 15:06:43 +0200

    feat(l10n): add Japanese localization

--- Apps/Keyty/Project.swift
+++ Apps/Keyty/Project.swift
@@ -171,7 +171,7 @@ let project = Project(
     name: "Keyty",
     organizationName: "Keyty",
     options: .options(
-        defaultKnownRegions: ["de", "en", "es", "uk"],
+        defaultKnownRegions: ["de", "en", "es", "ja", "uk"],
         developmentRegion: "en"
     ),
     packages: [
